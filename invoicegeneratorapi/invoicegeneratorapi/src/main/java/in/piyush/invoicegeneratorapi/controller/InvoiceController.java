692b8ae8f54db7142e50817fa339d121123ba68d
Piyush0434 <piyushghatage3@gmail.com>
2025-07-19 20:16:40 +0530

    Solved the API error for saving the invoice details in database

--- invoicegeneratorapi/invoicegeneratorapi/src/main/java/in/piyush/invoicegeneratorapi/controller/InvoiceController.java
+++ invoicegeneratorapi/invoicegeneratorapi/src/main/java/in/piyush/invoicegeneratorapi/controller/InvoiceController.java
@@ -4,14 +4,12 @@ import in.piyush.invoicegeneratorapi.entity.Invoice;
 import in.piyush.invoicegeneratorapi.service.InvoiceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/invoices")
+@CrossOrigin("*")
 public class InvoiceController {
 
     private final InvoiceService invoiceService;
