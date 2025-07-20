4a0ce68efa7e2c516677fa38d4241f74a7fe1e88
Piyush0434 <piyushghatage3@gmail.com>
2025-07-20 23:14:48 +0530

    feature to send emails to client added

--- invoicegeneratorapi/invoicegeneratorapi/src/main/java/in/piyush/invoicegeneratorapi/controller/InvoiceController.java
+++ invoicegeneratorapi/invoicegeneratorapi/src/main/java/in/piyush/invoicegeneratorapi/controller/InvoiceController.java
@@ -1,10 +1,13 @@
 package in.piyush.invoicegeneratorapi.controller;
 
 import in.piyush.invoicegeneratorapi.entity.Invoice;
+import in.piyush.invoicegeneratorapi.service.EmailService;
 import in.piyush.invoicegeneratorapi.service.InvoiceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -15,6 +18,7 @@ import java.util.List;
 public class InvoiceController {
 
     private final InvoiceService invoiceService;
+    private final EmailService emailService;
 
     @PostMapping
     public ResponseEntity<Invoice> saveInvoice(@RequestBody Invoice invoice){
@@ -30,5 +34,17 @@ public class InvoiceController {
     public ResponseEntity<Void> removeInvoice(@PathVariable String id){
         invoiceService.removeInvoice(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/sendinvoice")
+    public ResponseEntity<?> sendInvoice(@RequestPart("file")MultipartFile file,
+                                         @RequestPart("email")String customerEmail){
+        try{
+            emailService.sendInvoiceEmail(customerEmail,file);
+            return ResponseEntity.ok().body("Invoice sent successfully");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to send invoice");
+        }
+
     }
 }
